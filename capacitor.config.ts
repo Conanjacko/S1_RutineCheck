d518516ec9308cccd2bd9db65ce1d5bf15ba6c90
Conanjacko <pau.millan@duocuc.cl>
2024-05-29 16:50:33 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'S1_RutineCheck',
+  webDir: 'www'
+};
+
+export default config;
